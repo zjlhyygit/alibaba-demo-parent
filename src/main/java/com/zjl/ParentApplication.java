14da55c3229f3bcce760708f79a7a1c0e71fe2f0
unknown <937432721@qq.com>
2022-07-17 22:53:06 +0800

    parent

--- src/main/java/com/zjl/ParentApplication.java
+++ src/main/java/com/zjl/ParentApplication.java
@@ -1,3 +1,11 @@
+/*
+ * @Author: zhaojl
+ * @Date: 2022-07-17 22:50:34
+ * @LastEditTime: 2022-07-17 22:51:49
+ * @LastEditors: zhaojl
+ * @Description: 
+ * @FilePath: \undefinedd:\code\alibabaDemo\parent\src\main\java\com\zjl\ParentApplication.java
+ */
 package com.zjl;
 
 import org.springframework.boot.SpringApplication;
@@ -7,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ParentApplication {
 
     public static void main(String[] args) {
+        // syso
         SpringApplication.run(ParentApplication.class, args);
     }
 
